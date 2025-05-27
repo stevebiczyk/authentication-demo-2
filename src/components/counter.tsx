938df1361696059e12fd3c96d07df148cf9547a8
stevebiczyk <stevebiczyk@gmail.com>
2025-05-27 23:03:01 +0100

    implemented role based access control

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,27 +1,31 @@
 "use client";
 
 import { useState } from "react";
-import { useAuth, useUser } from "@clerk/nextjs";
+import {
+  useAuth,
+  // useUser
+} from "@clerk/nextjs";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
+  const {
+    isLoaded,
+    userId,
+    // sessionId, getToken
+  } = useAuth();
+  //   const { isLoaded, isSignedIn, user } = useUser();
 
-  const { isLoaded, userId, sessionId, getToken } = useAuth();
-  const { isLoaded, isSignedIn, user } = useUser();
-  if (!isLoaded || !isSignedIn) {
-    // If auth or user data is not loaded or user is not signed in, return null
+  if (!isLoaded || !userId) {
     return null;
   }
 
+  //   if (!isLoaded || !isSignedIn) {
+  //     return null;
+  //   }
   return (
     <>
       <p>Count: {count}</p>
-      <button
-        onClick={() => setCount(count + 1)}
-        className="ml-2 px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Increment
-      </button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </>
   );
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,22 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 import { createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 // const isProtectedRoute = createRouteMatcher(["/user-profile"]);
 const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+
 export default clerkMiddleware(async (auth, req) => {
   const { userId, redirectToSignIn } = await auth();
+
+  if (
+    isAdminRoute(req) &&
+    (await auth()).sessionClaims?.metadata.role !== "admin"
+  ) {
+    const url = new URL("/", req.url);
+    return NextResponse.redirect(url);
+  }
   if (!userId && !isPublicRoute(req)) {
     // If the user is not authenticated and the route is not public, redirect to sign-in
     return redirectToSignIn();
